be4bed88c5c28e8ae752cefa79bd2f23bba006a9
Rodel60 <Rodel60@users.noreply.github.com>
2015-06-08 01:53:47 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "window.h"
 #include "image.h"
 #include "atlas.h"
+#include "level.h"
 
 Render * render; //needs to be global because GLFW is pure C
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
@@ -10,6 +11,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 int main() {
 	Image image = Image("resources/atlas.png");
 	Atlas atlas = Atlas("resources/atlasMap.csv");
+	Level level = Level("resources/test_level.csv");
 
 	GLFWwindow* window = createWindow(640, 480, WINDOWED);
 	if (!window)
@@ -20,11 +22,48 @@ int main() {
 
 		render = new Render(64, 32, image.getImage_data(), window);
 
+		if (level.getWidth() < 640 || level.getHeight() < 480 ||
+		 level.getHeight() < 0 || level.getWidth() < 0) {
+			std::cerr << "ERROR: Level size is less than window size." << std::endl;
+		}
+
+		// Not sure how to implement full level size, 
+		// data is stored as X = level.getWidth(), Y = level.getHeight()
+
 		//bottom left = (0, 0)
-		render->addImage(40, 2, atlas.getCoords("test"));
-		render->addImage(300, 93, atlas.getCoords("test3"));
+
+		// Load Pancho
+		if (level.start("X") > level.getWidth() || level.start("Y") > level.getHeight() ||
+		 level.start("X") < 0 || level.start("Y") < 0) {
+			std::cerr << "ERROR: Pancho is out of this world." << std::endl;
+		}
+		render->addImage(level.start("X"), level.start("Y"), atlas.getCoords("test3"));
+
+		// Load goal
+		if (level.goal("X") > level.getWidth() || level.goal("Y") > level.getHeight() ||
+		 level.goal("X") < 0 || level.goal("Y") < 0) {
+			std::cerr << "ERROR: Goal is out of this world." << std::endl;
+		}
+		render->addImage(level.goal("X"), level.goal("Y"), atlas.getCoords("test"));
+
+		// Load enemies
+		for (std::map<Point, std::string>::const_iterator enemies = level.getEnemies().begin(); enemies != level.getEnemies().end(); ++enemies) {
+			if (enemies->first.pt_X > level.getWidth() || enemies->first.pt_Y > level.getHeight() ||
+		 		enemies->first.pt_X < 0 || enemies->first.pt_Y < 0) {
+				std::cerr << "ERROR: Enemy is not within level bounds." << std::endl;
+			}
+		render->addImage(enemies->first.pt_X, enemies->first.pt_Y, atlas.getCoords("test"));
+		}
+
+		// Load Power-Ups
+		for (std::map<Point, std::string>::const_iterator items = level.getPower_Ups().begin(); items != level.getPower_Ups().end(); ++items) {
+			if (items->first.pt_X > level.getWidth() || items->first.pt_Y > level.getHeight() ||
+		 		items->first.pt_X < 0 || items->first.pt_Y < 0) {
+				std::cerr << "ERROR: Item is not within level bounds." << std::endl;
+			}
+		render->addImage(items->first.pt_X, items->first.pt_Y, atlas.getCoords("test3"));
+		}
 		render->render(60);
-		
 	}
 
 	delete(render);
