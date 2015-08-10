bfc675bdd62b5967d6473d3a0eca78cc0898cec7
Brian Kovacik <varnishedgreen@aim.com>
2015-08-10 16:59:11 -0700

    readme changed

--- src/objects/factory.cpp
+++ src/objects/factory.cpp
@@ -1,12 +1,13 @@
 #include "factory.h"
 
-std::map<std::string, funcptr> Factory::funcmap_;
+std::map<std::string, funcptr4> Factory::funcmap4_;
+std::map<std::string, funcptr2> Factory::funcmap2_;
 Factory::InitFactory Factory::init;
 
 Factory::InitFactory::InitFactory() {
-	funcmap_["pancho"] = Factory::createObject<Pancho>;
-	funcmap_["block"] = Factory::createObject<Block>;
-	funcmap_["crumble"] = Factory::createObject<Crumble>;
-	funcmap_["spikes"] = Factory::createObject<Spikes>;
-	funcmap_["jumpthrough"] = Factory::createObject<JumpThrough>;
+	funcmap4_["pancho"] = Factory::createObject<Pancho>;
+	funcmap4_["block"] = Factory::createObject<Block>;
+	funcmap4_["crumble"] = Factory::createObject<Crumble>;
+	funcmap4_["spikes"] = Factory::createObject<Spikes>;
+	funcmap4_["jumpthrough"] = Factory::createObject<JumpThrough>;
 }
--- src/objects/factory.h
+++ src/objects/factory.h
@@ -13,10 +13,12 @@
 
 class Factory;
 
-typedef GameObject* (*funcptr)(int, int, int, int);
+typedef GameObject* (*funcptr4)(int, int, int, int);
+typedef GameObject* (*funcptr2)(int, int);
 
 class Factory {
-	static std::map<std::string, funcptr> funcmap_;
+	static std::map<std::string, funcptr4> funcmap4_;
+	static std::map<std::string, funcptr2> funcmap2_;
 
 	static class InitFactory {
 		public:
@@ -24,11 +26,11 @@ class Factory {
 	} init;
 
 	template<typename T> static GameObject* createObject(int a0, int a1, int a2, int a3) {return new T(a0, a1, a2, a3);}
+	template<typename T> static GameObject* createObject(int a0, int a1) {return new T(a0, a1);}
 
 	public:
-		static GameObject* getNewObject(std::string type, int a0, int a1, int a2, int a3) {return funcmap_[type](a0, a1, a2, a3);}
-
-
+		static GameObject* getNewObject(std::string type, int a0, int a1, int a2, int a3) {return funcmap4_[type](a0, a1, a2, a3);}
+		static GameObject* getNewObject(std::string type, int a0, int a1) {return funcmap2_[type](a0, a1);}
 };
 
 #endif
